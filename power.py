09f6e8863d1689c1a182e4bee6e86f5dc9f1b08b
Bipeen Acharya <acharyab@seas.upenn.edu>
2019-11-04 08:26:13 -0800

    Update to create files

--- power.py
+++ power.py
@@ -4,23 +4,22 @@ import pandas as pd
 import csv
 
 df = pd.read_excel(io=file_name, sheet_name=sheet, skiprows=1)
-i = 0
-with open('output_python.csv', 'w') as writeFile:
-    writer = csv.writer(writeFile)
-    for index, row in df.iterrows():
-        new_row = [str(row['Layer Number']), 
-             str(row['Layer Soil Type']), 
-             str(row['Depth to Bottom of Soil Layer (feet)']), 
-             str(row['Total Unit Weight\n(2) (pcf)']), 
-             str(row['Deformation Modulus (ksi)']), 
-             str(row['Effective Friction Angle (degrees)']),
-             str(row['Undrained Shear Strength or Rock Eff. Cohesion (ksf)']),
-             str(row['Rock/Concrete Ultimate Bond Strength (ksf) (3)'])]
-        print(new_row)
-        if str(row['Boring Number']).startswith('B-'):
+rows = []
+for index, row in df.iterrows():
+    b_row = ""
+    if str(row['Boring Number']).startswith('B-'):
+        with open(row['Boring Number']+'.csv', 'w') as writeFile:
+            writer = csv.writer(writeFile)
             b_row = [str(row['Boring Number'])]
             writer.writerow(b_row)
-        writer.writerow(new_row)
-        i+=1
-        if i == 20:
-            break
+            for r in rows:
+                writer.writerow(r)
+    new_row = [str(row['Layer Soil Type']), 
+         str(row['Depth to Bottom of Soil Layer (feet)']), 
+         str(row['Total Unit Weight\n(2) (pcf)']), 
+         str(row['Deformation Modulus (ksi)']), 
+         str(row['Effective Friction Angle (degrees)']),
+         str(row['Undrained Shear Strength or Rock Eff. Cohesion (ksf)']),
+         str(row['Rock/Concrete Ultimate Bond Strength (ksf) (3)'])]
+
+    rows.append(new_row)
